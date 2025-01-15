e9cd0dbc8f878e55b19952da0e21b466f4d52ce3
Fmeat51 <Fmeat51@gmail.com>
2025-01-15 20:59:11 -0300

    Create README.lua

---
+++ README.lua
@@ -0,0 +1,2 @@
+--[[FULL CREDITS TO UKIYO FOR DEVELOPING THE ORIGINAL MORPH UI]]
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Fmeat51/loweruncuiiguess/refs/heads/main/thosewhoknow.lua'))();
