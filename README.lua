3f34d8b141390a2710f0d29df13e0258a0a65707
Fmeat51 <Fmeat51@gmail.com>
2025-01-15 21:11:31 -0300

    Update and rename README.lua to README.md

--- README.lua
+++
@@ -1,9 +0,0 @@
---[[
-
-FULL CREDITS TO UKIYO FOR DEVELOPING THE ORIGINAL MORPH UI
-
-ALL I DID WAS CHANGE SOME STUFF
-]]
-
-local success, message = pcall(loadstring(game:HttpGet('https://raw.githubusercontent.com/Fmeat51/loweruncuiiguess/refs/heads/main/thosewhoknow.lua')))
-if not success and message then warn(message); end
