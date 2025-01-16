bdeaad82ec1b1660bb17d6b34ed3f037db9308cb
Fmeat51 <Fmeat51@gmail.com>
2025-01-15 21:11:49 -0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,2 @@
+local success, message = pcall(loadstring(game:HttpGet('https://raw.githubusercontent.com/Fmeat51/loweruncuiiguess/refs/heads/main/thosewhoknow.lua')))
+if not success and message then warn(message); end
