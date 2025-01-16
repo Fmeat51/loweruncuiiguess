3d8686949d11a860dfbe0ee39609bc23609ae0c2
Fmeat51 <Fmeat51@gmail.com>
2025-01-15 21:09:39 -0300

    Update README.lua

--- README.lua
+++ README.lua
@@ -4,4 +4,6 @@ FULL CREDITS TO UKIYO FOR DEVELOPING THE ORIGINAL MORPH UI
 
 ALL I DID WAS CHANGE SOME STUFF
 ]]
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fmeat51/loweruncuiiguess/refs/heads/main/thosewhoknow.lua'))();
+
+local success, message = pcall(loadstring(game:HttpGet('https://raw.githubusercontent.com/Fmeat51/loweruncuiiguess/refs/heads/main/thosewhoknow.lua')))
+if not success and message then warn(message); end
